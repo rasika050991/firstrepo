482b0ff3696d8a4be51a1aa90f1fd62cd7c0f63f
RASIKA <RASIKA@RASIKA-PC>
2020-09-05 13:48:29 +0530

    Commit by Rasika

--- GIT_Demo/src/com/cjc/Demo.java
+++ GIT_Demo/src/com/cjc/Demo.java
@@ -2,9 +2,18 @@ package com.cjc;
 
 public class Demo {
 	
+	
+	
+	public void m1()
+	{
+		
+		System.out.println("I made changes......");
+	}
+	
+	
 	public static void main(String[] args) {
 		
-		
+		System.out.println("Welcome to CJC........");
 		
 		
 		System.out.println("This is my first GIT program");
